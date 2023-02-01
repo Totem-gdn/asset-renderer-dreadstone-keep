672f262914795012577034a9227dd48ab45adb89
illiadimension <ilya.nikulin@it-dimension.com>
2023-02-01 13:43:26 +0200

    add reverse api url

--- helpers/dna-parser.js
+++ helpers/dna-parser.js
@@ -7,6 +7,8 @@ const maxValue = 4294967295;
 class NFT {
   constructor() {
     this.ApiURL = process.env.API_URL;
+    this.ReverseApiURL = process.env.RESERVE_API_URL;
+
     this.Contracts = {
       avatar: process.env.AVATAR_CONTRACT,
       item: process.env.ITEM_CONTRACT,
@@ -16,8 +18,14 @@ class NFT {
   async get (type, id) {
     try {
 
-      const contractHandler = new ContractHandler(this.ApiURL, this.Contracts[type]);
-      const dna = await contractHandler.getDNA(id);
+      let dna;
+      try {
+        const contractHandler = new ContractHandler(this.ApiURL, this.Contracts[type]);
+        dna = await contractHandler.getDNA(id);
+      } catch (error) {
+        const contractHandler = new ContractHandler(this.ReverseApiURL, this.Contracts[type]);
+        dna = await contractHandler.getDNA(id);
+      }
       let parser;
       if (type === 'avatar') {
         parser = new DNAParser(totemCommonFiles.totemAvatarDreadstoneKeepFilterJson, dna);
